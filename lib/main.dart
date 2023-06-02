055eeaae42201f9462dc033cbdae617a1beb1bad
KhrisnaIndrawan <khrisna.indrawan@gmail.com>
2023-06-02 22:44:36 +0700

    add color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:dicoding_news_app/newslist_page.dart';
 import 'package:dicoding_news_app/detail_page.dart';
 import 'package:dicoding_news_app/model/article.dart';
 import 'package:dicoding_news_app/article_webview.dart';
+import 'package:dicoding_news_app/styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,8 +17,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'News App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: primaryColor,
+              onPrimary: Colors.black,
+              secondary: secondaryColor,
+            ),
       ),
       initialRoute: NewsListPage.routeName,
       routes: {
